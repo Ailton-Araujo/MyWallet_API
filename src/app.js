7521169b2c58f2ae364f38bfd7a3d20e2a2291a6
Ailton Araujo <ailton.correia.araujo@gmail.com>
2023-07-04 19:43:26 -0300

    Fix Sign-in

--- src/app.js
+++ src/app.js
@@ -79,7 +79,7 @@ app.post("/sign-in", async (req, res) => {
   }
   try {
     const user = await db.collection("users").findOne({ email });
-    if (user) return res.status(404).send("Usuário Não cadastrado");
+    if (!user) return res.status(404).send("Usuário Não cadastrado");
 
     if (bcrypt.compareSync(password, user.password)) {
       const token = uuid();
