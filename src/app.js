e0a91e51275d50e3d6f2fb09f217d6db34d86b20
Ailton Araujo <ailton.correia.araujo@gmail.com>
2023-07-04 19:37:41 -0300

    Sign-in

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import { MongoClient, ObjectId } from "mongodb";
 import dotenv from "dotenv";
 import Joi from "joi";
 import bcrypt from "bcrypt";
+import { v4 as uuid } from "uuid";
 
 // Server Create
 const app = express();
@@ -28,8 +29,13 @@ const db = mongoClient.db();
 
 //DataSchema
 
-const userSchema = Joi.object({
+const signUpSchema = Joi.object({
   name: Joi.string().required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().min(3).required(),
+});
+
+const signInSchema = Joi.object({
   email: Joi.string().email().required(),
   password: Joi.string().min(3).required(),
 });
@@ -41,7 +47,7 @@ const userSchema = Joi.object({
 app.post("/sign-up", async (req, res) => {
   const { name, email, password } = req.body;
 
-  const validation = userSchema.validate(req.body, { abortEarly: false });
+  const validation = signUpSchema.validate(req.body, { abortEarly: false });
 
   if (validation.error) {
     const errors = validation.error.details.map((detail) => detail.message);
@@ -55,6 +61,33 @@ app.post("/sign-up", async (req, res) => {
 
     await db.collection("users").insertOne({ name, email, password: hash });
     res.sendStatus(201);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+//Sign-in
+
+app.post("/sign-in", async (req, res) => {
+  const { email, password } = req.body;
+
+  const validation = signInSchema.validate(req.body, { abortEarly: false });
+
+  if (validation.error) {
+    const errors = validation.error.details.map((detail) => detail.message);
+    return res.status(422).send(errors);
+  }
+  try {
+    const user = await db.collection("users").findOne({ email });
+    if (user) return res.status(404).send("Usuário Não cadastrado");
+
+    if (bcrypt.compareSync(password, user.password)) {
+      const token = uuid();
+      await db.collection("sessions").insertOne({ userId: user._id, token });
+      res.status(200).send(token);
+    } else {
+      res.status(401).send("Senha Incorreta");
+    }
   } catch (err) {
     res.status(500).send(err.message);
   }
