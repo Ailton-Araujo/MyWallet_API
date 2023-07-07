3cda6f7c7c97140b15a00f97a2bc1697e0d89cce
Ailton Araujo <ailton.correia.araujo@gmail.com>
2023-07-06 22:12:26 -0300

    fix authMiddleware

--- src/middlewares/validateAuth.js
+++ src/middlewares/validateAuth.js
@@ -8,9 +8,9 @@ export default async function validateAuth(req, res, next) {
   try {
     const session = await db.collection("sessions").findOne({ token });
     if (!session) return res.sendStatus(401);
+    res.locals.userId = session.userId;
   } catch (err) {
     res.status(500).send(err.message);
   }
-  res.locals.userId = session.userId;
   next();
 }
