a79ff7c40ffb75de99cad39a561a3fe402d07e9f
Ailton Araujo <ailton.correia.araujo@gmail.com>
2023-07-08 21:32:51 -0300

    Fix SchemaTransaction

--- src/schemas/transactions.Schemas.js
+++ src/schemas/transactions.Schemas.js
@@ -2,7 +2,7 @@ import Joi from "joi";
 
 const transactionSchema = Joi.object({
   description: Joi.string().required(),
-  amount: Joi.number().precision(2).greater(0),
+  amount: Joi.number().precision(2).greater(0).required(),
   type: Joi.any().valid("entrada", "saida").required(),
 });
 
